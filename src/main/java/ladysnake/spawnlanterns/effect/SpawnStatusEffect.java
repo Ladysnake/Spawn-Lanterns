05d197c5d5df29bc125a700865de2b766d1d27f0
doctor4t <25477005+doctor4t@users.noreply.github.com>
2020-11-04 01:39:42 +0100

    Add soothing and provocation status effects

---
+++ src/main/java/ladysnake/spawnlanterns/effect/SpawnStatusEffect.java
@@ -0,0 +1,10 @@
+package ladysnake.spawnlanterns.effect;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectType;
+
+public class SpawnStatusEffect extends StatusEffect {
+    public SpawnStatusEffect(StatusEffectType type, int color) {
+        super(type, color);
+    }
+}
