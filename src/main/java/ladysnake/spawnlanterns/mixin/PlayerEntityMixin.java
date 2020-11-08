b7f21818984a6c1e88d820d9d14cf23f5fd0e5ab
doctor4t <25477005+doctor4t@users.noreply.github.com>
2020-11-08 22:08:27 +0100

    Fix hosile mobs targeting creative players and switching targets every tick

--- src/main/java/ladysnake/spawnlanterns/mixin/PlayerEntityMixin.java
+++ src/main/java/ladysnake/spawnlanterns/mixin/PlayerEntityMixin.java
@@ -10,12 +10,15 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
+    @Shadow public abstract boolean isCreative();
+
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -24,11 +27,11 @@ public abstract class PlayerEntityMixin extends LivingEntity {
     public void tick(CallbackInfo ci) {
         if (this.hasStatusEffect(SpawnLanterns.PROVOCATION)) {
             this.getEntityWorld().getEntitiesByClass(LivingEntity.class, this.getBoundingBox().expand(32.0D), EntityPredicates.VALID_LIVING_ENTITY).forEach(entity -> {
-                if (entity instanceof Angerable) {
+                if (entity instanceof Angerable && ((Angerable) entity).getTarget() != null && !this.isCreative()) {
                     ((Angerable) entity).setAngryAt(this.getUuid());
                     ((Angerable) entity).setTarget(this);
                 }
-                if (entity instanceof HostileEntity) {
+                if (entity instanceof HostileEntity && ((HostileEntity) entity).getTarget() != null && !this.isCreative()) {
                     ((HostileEntity) entity).setTarget(this);
                 }
             });
